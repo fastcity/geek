1656b94f5d7b5150c84192fbe33aa3d6427ca3a5
goujiangfei <l178802263@qq.com>
2019-10-24 23:38:50 +0800

    链表代码

--- data/lru/lru.js
+++ data/lru/lru.js
@@ -1,8 +1,6 @@
 
 
 // 数组 实现 思路： 找到之后放到最后一个，满了之后删除第一个
-
-
 class LRU {
     constructor(len) {
         this.len = len
@@ -36,7 +34,6 @@ class LRU {
     }
     print() {
         console.log(this.arr);
-
     }
 
 }
@@ -52,4 +49,64 @@ lru.push(2)
 lru.push(4)
 lru.push(2)
 
-lru.print()
+// lru.print()
+
+
+
+
+
+class linkNode {
+    constructor() {
+        this.head = null
+        this.end = null
+        this.len = 0
+    }
+
+    createNode(data) {
+        return {
+            data,
+            next: null
+        }
+    }
+
+    add(node) {
+
+        if (this.head == null) {
+            this.head = node
+            this.end = node
+        } else {
+            let current = this.head
+
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node 
+
+            // this.end.next = node
+            // this.head.next = this.end
+            // this.end = node
+        }
+        // this.head = current
+        this.len++
+    }
+
+    delete(data) {
+
+    }
+
+    print() {
+        console.log("--", this.head);
+
+    }
+}
+
+const link = new linkNode()
+
+
+link.add(link.createNode(1))
+
+link.add(link.createNode(2))
+link.add(link.createNode(3))
+link.add(link.createNode(4))
+
+link.print()
